edf8f30176cef2801c2c765e49702d847eae6008
BurntCr1sp <ben.richards@npbhs.school.nz>
2024-08-17 14:48:16 +1200

    for some reason print isnt printing.... im so confused

--- NCEA-LOTR-QUIZ.py
+++ NCEA-LOTR-QUIZ.py
@@ -1,12 +1,23 @@
 import time # Imports time to be used later in the code
 import os # Imports os to be used later in the code
 
+# Constants
+TITLE = 'LORD OF THE RINGS QUIZ'
+
+print('')
+print(TITLE)
+print('')
+
 # The questions, options to awnser and awnsers for the quiz all put in lists
 questions = [1,"Who possesses the ring of power at the start of the trilogy?",2,"Who do the hobbits encounter at the Prancing Pony?",3,"How many members make up the fellowship of the ring?",4,"What is the name of the elf in the fellowship?",5,"What is the name of the dwarf in the fellowship?",6,"Who is the first member of the fellowship to be separated from the group?",7,"What happens at the end of the first movie?",8,"Where is Sarumans tower, Orthanc, located?",9,"Who has been following Sam and Frodo?",10,"Who is the king of Rohan?"] 
 options = [1,"A. Bilbo B. John C. Dumbledoor D. Humblemoore E. stewie griffin",2,"A. The manager of the store B. Their cousin C. A prancing pony D. Legoland E. Strider",3,"A. 1.5 B. 10 C. enough D. 9 E. 7",4,"A. Buddy B. Legolas C. Jolly D. Bernard E. Aegnor",5,"A. Peter Dinklage B.Tommy Morrison C. Gimli D. Balin E. Dwalin",6,"A. Gandalf B. Jerry the stone C. The ring D. Frodo E. Aragorn",7,"A. Merry and Pippin are carried off by orcs and Uruk-hai B. Boromir is killed C. Frodo and Sam leave the others behind D. All of the above",8,"A. Minas Tirith B. Rivendell C. Isengard D. Helms Deep E. Next to his grandmas house",9,"A. Gollum B. Sméagol C. A and B D. None of the above E. Max Verstappen",10,"A. Wormtongue B. Éowyn C. Éomer D. Sméagol E. Théoden"]
 awnsers = [1,"A",2,"E",3,"D",4,"B",5,"C",6,"A",7,"D",8,"C",9,"A",10,"E"]
+corrects = []
 
 def question1(): # This function controls the first question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[1])
     print('')
     print(options[1])
@@ -14,12 +25,16 @@ def question1(): # This function controls the first question and all its options
     Q1 = input('Your awnser: ')
     if Q1 == awnsers[1]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[1])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question2(): # This function controls the second question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[3])
     print('')
     print(options[3])
@@ -27,12 +42,16 @@ def question2(): # This function controls the second question and all its option
     Q2 = input('your awnser: ')
     if Q2 == awnsers[3]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[3])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question3(): # This function controls the third question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[5])
     print('')
     print(options[5])
@@ -40,12 +59,16 @@ def question3(): # This function controls the third question and all its options
     Q3 = input('your awnser: ')
     if Q3 == awnsers[5]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[5])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question4(): # This function controls the fourth question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[7])
     print('')
     print(options[7])
@@ -53,12 +76,16 @@ def question4(): # This function controls the fourth question and all its option
     Q4 = input('your awnser: ')
     if Q4 == awnsers[7]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[7])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question5(): # This function controls the fifth question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[9])
     print('')
     print(options[9])
@@ -66,12 +93,16 @@ def question5(): # This function controls the fifth question and all its options
     Q5 = input('your awnser: ')
     if Q5 == awnsers[9]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[9])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question6(): # This function controls the sixth question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[11])
     print('')
     print(options[11])
@@ -79,12 +110,16 @@ def question6(): # This function controls the sixth question and all its options
     Q6 = input('your awnser: ')
     if Q6 == awnsers[11]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[11])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question7(): # This function controls the seventh question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[13])
     print('')
     print(options[13])
@@ -92,12 +127,16 @@ def question7(): # This function controls the seventh question and all its optio
     Q3 = input('your awnser: ')
     if Q3 == awnsers[13]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[13])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question8(): # This function controls the eighth question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[15])
     print('')
     print(options[15])
@@ -105,12 +144,16 @@ def question8(): # This function controls the eighth question and all its option
     Q4 = input('your awnser: ')
     if Q4 == awnsers[15]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[15])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question9(): # This function controls the ninth question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[17])
     print('')
     print(options[17])
@@ -118,12 +161,16 @@ def question9(): # This function controls the ninth question and all its options
     Q5 = input('your awnser: ')
     if Q5 == awnsers[17]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[17])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
 def question10(): # This function controls the tenth question and all its options & awnsers
+    print('')
+    print(TITLE)
+    print('')
     print(questions[19])
     print('')
     print(options[19])
@@ -131,6 +178,7 @@ def question10(): # This function controls the tenth question and all its option
     Q6 = input('your awnser: ')
     if Q6 == awnsers[19]:
         print('Correct')
+        corrects.append('Correct')
     else:
         print('Incorrect, the awnser was', awnsers[19])
     time.sleep(4) # Makes the program wait 4 seconds before continuing to run
@@ -158,23 +206,52 @@ def startQ2(): # This function controls the starting question 2
         time.sleep(3)# Makes the program wait 3 seconds before continuing to run
     os.system('clear') # Clears the terminal which helps to seperate questions and make things look less messy
 
+def passfail():
+    look_for = 'Correct'
+    score = corrects.count(look_for)
+
+    if user_num == 6 and score <= 3:
+        print('You got a score of',score,)
+        print('Fail')
+
+    elif user_num == 6 and score > 3:
+        print('You got a score of',score,)
+        print('Pass')
+
+    elif user_num == 10 and score <= 5:
+        print('You got a score of',score,)
+        print('Fail')
+
+    elif user_num == 10 and score > 5:
+        print('You got a score of',score,)
+        print('Pass')
+
 def end(): # This function controls the ending to see if the user would like to start over
 
     end = True
-    
+
+    print('')
+    print(TITLE) # Prints the title
+    print('')
+
+    passfail()
+
     again = input("Would you like to play again? ").lower()
 
     while end == True:
         if again == "yes":
+            corrects.clear() # Clears everything from the corrects list
             amountQ1() # goes to the amountQ1 function to follow its code
         if again == "no":
+            corrects.clear() # Clears everything from the corrects list
             exit() # Exits and ends the program
         again = input("Would you like to play again? ").lower()
 
 def amountQ1(): # This function controls when and where everything is in the order
 
     amountQ1 = True
-    
+
+    global user_num
     user_num = input("Would you like 6 questions or 10? ")
 
     while amountQ1 == True:
